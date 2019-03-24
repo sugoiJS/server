5dcfc76a101f1445274f6957c13f36820d1c0a07
Orel Balilti <orelba93@gmail.com>
2019-03-24 22:35:07 +0200

    feat: Support injector class for ease the IoC handling

---
+++ classes/injector.class.ts
@@ -0,0 +1,27 @@
+import {Container, Injectable, interfaces} from "@sugoi/core";
+
+@Injectable()
+export class Injector{
+    private static Container: Container;
+    private _container: Container;
+
+    constructor(
+    ){
+        this._container = Injector.Container;
+        const proxy = new Proxy(this._container,{});
+        Object.assign(this, proxy);
+    }
+
+
+    static setContainer(container: Container) {
+        this.Container = container;
+    }
+
+    get<T = any>(serviceIdentifier: interfaces.ServiceIdentifier<T>): T {
+        return this._container.get<T>(serviceIdentifier);
+    }
+
+    resolve<T = any>(constructorFunction: interfaces.Newable<T>): T{
+        return this._container.resolve<T>(constructorFunction);
+    }
+}
